b6af8c5b99b9a58a019191b176d58d3e71b63007
NoOneISeQual <kurdefrance1613@gmail.com>
2023-06-03 19:20:44 +0300

    add recursive function

--- task2.py
+++ task2.py
@@ -8,3 +8,8 @@
 
 a_number = int(input('Введите первое число: '))
 b_number = int(input('Введите второе число: '))
+
+def rec_sum(a_number,b_number):
+    if a_number == 0:
+        return b_number
+    return rec_sum(a_number -1,b_number +1)
